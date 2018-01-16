c5ac326f5d1f2e4c43f95933d93e978b636c0fe6
esp <swetha.priya.eddula@sap.com>
2018-01-16 14:31:11 +0530

    modified clean_tweeets.py. It cleans each tweet and writes in respective files of basic emotions.

--- clean_tweets.py
+++ clean_tweets.py
@@ -3,13 +3,29 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import re
 
+
+def clean(text):
+	if(text[0:3].decode("utf-8") == "rt "):
+		text = text[3:]
+	text = text.decode("utf-8").encode("ascii", errors="ignore").decode()
+	text = ' '.join(list(filter(lambda x:x[0]!='@', text.split())))
+	return text
+
 def word_in_text(word, text):
-    word = word.lower()
-    text = text.lower()
-    match = re.search(word, text.decode('utf-8'))
-    if match:
-        return text
-    return ""
+	word = word.lower()
+	text = text.lower()
+	text = clean(text)
+	match = re.search(word, text)
+	if match:
+		return text
+	return ""
+
+HAPPY = open("happy_tweets.txt","a")
+SAD = open("sad_tweets.txt","a")
+ANGRY = open("angry_tweets.txt","a")
+FEAR = open("fear_tweets.txt","a")
+SURPRISE = open("surprise_tweets.txt","a")
+DISGUST = open("disgust_tweets.txt","a")
 
 tweets_data_path = 'sample_tweets.txt'
 
@@ -40,3 +56,28 @@ tweets['angry'] = tweets['text'].apply(lambda tweet: word_in_text('angry', tweet
 tweets['surprise'] = tweets['text'].apply(lambda tweet: word_in_text('surprise', tweet), tweets_data)
 tweets['disgust'] = tweets['text'].apply(lambda tweet: word_in_text('disgust', tweet), tweets_data)
 tweets['fear'] = tweets['text'].apply(lambda tweet: word_in_text('fear', tweet), tweets_data)
+
+for i in tweets['happy']:
+	HAPPY.write(i)
+
+for i in tweets['sad']:
+	SAD.write(i)
+
+for i in tweets['fear']:
+	FEAR.write(i)
+
+for i in tweets['surprise']:
+	SURPRISE.write(i)
+
+for i in tweets['disgust']:
+	DISGUST.write(i)
+
+for i in tweets['angry']:
+	ANGRY.write(i)
+
+HAPPY.close()
+SURPRISE.close()
+SAD.close()
+FEAR.close()
+ANGRY.close()
+DISGUST.close()
